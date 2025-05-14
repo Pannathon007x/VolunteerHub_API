13f348102e73d910852d3f33a5236b0d108fa227
Pannathon007x <158440872+Pannathon007x@users.noreply.github.com>
2025-05-14 11:34:34 +0700

    วางโครง Backend

--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,13 @@
 const bcrypt = require('bcrypt');
 
 
-const hashedAdminPass = bcrypt.hashSync('adminpass123', 10);  // admin password
+const hashedAdminPass = bcrypt.hashSync('admin41299', 10);  // admin password
 const hashedUserPass = bcrypt.hashSync('userpass123', 10);    // user password
  
 const users = [
     {
         id: 1,
-        email: 'admin@example.com',
+        email: 'byte9022@gmail.com',
         password: hashedAdminPass,  // hashed password
         name: 'Admin User',
         role: 'admin'  // admin role
