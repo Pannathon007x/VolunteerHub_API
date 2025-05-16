80c07895c1b0f59fd828bbc2d7a48930c8943120
Pannathon007x <158440872+Pannathon007x@users.noreply.github.com>
2025-05-16 10:53:38 +0700

    fix push token

--- app.js
+++ app.js
@@ -4,19 +4,17 @@ const cors = require('cors');
 
 const app = express();
 
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const activityRoutes = require('./routes/activityRoutes');
+
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
 // Routes
-// auth routes
-const authRoutes = require('./routes/authRoutes');
 app.use('/auth', authRoutes);
-// activity routes
-const activityRoutes = require('./routes/activityRoutes');
 app.use('/activity', activityRoutes);
-
-const userRoutes = require('./routes/userRoutes');
 app.use('/user', userRoutes);
 
 const PORT = 3001;
