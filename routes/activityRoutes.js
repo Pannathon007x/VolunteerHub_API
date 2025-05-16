8ecfe55f87b0eadb4ca4e2e778d8de943cc0a594
Pannathon007x <158440872+Pannathon007x@users.noreply.github.com>
2025-05-16 17:05:50 +0700

    add อัปเดตสถานะกิจกรรมเป็น completed

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllActivities, getActivityById, getActivityByCompleted } = require('../controllers/activityControllers');
+const { getAllActivities, getActivityById, completeActivity } = require('../controllers/activityControllers');
 
-router.get('/completed', getActivityByCompleted);
+
 // GET /activity
 router.get('/get', getAllActivities);
 
 // GET /activity/:id
 router.get('/:id', getActivityById);
 
+// ทำกิจกรรมให้เสร็จ (อัปเดตสถานะ + ชั่วโมงจิตอาสา)
+router.put('/:id/complete', completeActivity);
 
 module.exports = router;
