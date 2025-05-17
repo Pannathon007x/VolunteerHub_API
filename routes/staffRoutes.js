bd1168ff49ce6be245bbd33113f4d8a5f6b9c8a9
iYiMz <yimtanaded@gmail.com>
2025-05-17 18:12:53 +0700

    maybefinish

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const db = require('../config/db'); // Import database connection
 db.connect();
 
-const { approveActivity, cancelActivity, createActivity } = require('../controllers/staffControllers');
+const { approveActivity, cancelActivity, createActivity, getActivityById, editActivity, getAllActivities } = require('../controllers/staffControllers');
 
 // route สำหรับ staff ใช้เหมือน admin แต่แยก controller ของ staff
 router.post('/post', createActivity);
 
 router.patch('/approve/:id', approveActivity);
+
 router.patch('/cancel/:id', cancelActivity);
+
+router.get('/showac/:id', getActivityById);
+
+router.patch('/editac/:id', editActivity);
+
+router.get('/getac', getAllActivities);
 
 module.exports = router;
